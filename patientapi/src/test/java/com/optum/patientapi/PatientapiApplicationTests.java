d36f182a5a95a88e6519335238c611f38b096e1b
eswaribala <parameswaribala@gmail.com>
2023-08-01 13:03:51 +0530

    updated

---
+++ patientapi/src/test/java/com/optum/patientapi/PatientapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.patientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
