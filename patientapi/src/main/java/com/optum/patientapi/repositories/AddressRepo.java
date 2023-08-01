300fe4013d4c32e2e5f3b9751936deda9e1dc1e7
eswaribala <parameswaribala@gmail.com>
2023-08-01 15:51:23 +0530

    updated

--- patientapi/src/main/java/com/optum/patientapi/repositories/AddressRepo.java
+++ patientapi/src/main/java/com/optum/patientapi/repositories/AddressRepo.java
@@ -1,7 +1,15 @@
 package com.optum.patientapi.repositories;
 
 import com.optum.patientapi.models.Address;
+import com.optum.patientapi.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AddressRepo extends JpaRepository<Address,Long> {
+
+    @Query("Select a from Address a where a.patient.adharCardNo=:adharCardNo")
+    public List<Address> findAddressByAdharCardNo(@Param("adharCardNo") String adharCardNo);
 }
--- patientapi/src/main/java/com/optum/patientapi/services/AddressService.java
+++ patientapi/src/main/java/com/optum/patientapi/services/AddressService.java
@@ -1,4 +1,72 @@
 package com.optum.patientapi.services;
 
+import com.optum.patientapi.models.Address;
+import com.optum.patientapi.models.Patient;
+import com.optum.patientapi.repositories.AddressRepo;
+import com.optum.patientapi.repositories.PatientRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.AbstractQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Service
 public class AddressService {
+
+    @Autowired
+    private AddressRepo addressRepo;
+
+    private PatientRepo patientRepo;
+
+
+    public Address addPatient(String adharCardNo, Address address){
+
+        Patient patient=this.patientRepo.findById(adharCardNo).orElse(null);
+        if(patient!=null){
+            address.setPatient(patient);
+            return this.addressRepo.save(address);
+
+        }
+
+        return null;
+    }
+
+    //getall
+
+    public List<Address> getAllAddresses(){
+        return this.addressRepo.findAll();
+    }
+
+    public List<Address> getAddressByAdharCardNo(String adharCardNo){
+        return this.addressRepo.findAddressByAdharCardNo(adharCardNo);
+    }
+
+
+
+    //update
+
+    public Address updateAddress(Address address){
+
+        return this.addressRepo.save(address);
+
+
+    }
+
+    //delete
+
+    public boolean deleteAddress(long addressId){
+        boolean status=false;
+        Address address=this.addressRepo.findById(addressId).orElse(null);
+        if(address!=null){
+            this.addressRepo.deleteById(addressId);
+            status=true;
+
+        }
+        return status;
+
+    }
 }
