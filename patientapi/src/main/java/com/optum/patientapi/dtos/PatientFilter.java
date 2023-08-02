e340560d298af0caf640469014fe1a8039c0ca0e
eswaribala <parameswaribala@gmail.com>
2023-08-02 14:33:25 +0530

    updated

--- patientapi/src/main/java/com/optum/patientapi/dtos/PatientFilter.java
+++ patientapi/src/main/java/com/optum/patientapi/dtos/PatientFilter.java
@@ -13,8 +13,6 @@ public class PatientFilter {
 
     private FilterField opId;
     private FilterField email;
-    private GenderFilterField gender;
+    //private GenderFilterField gender;
 
-    private List<PatientFilter> and;
-    private List<PatientFilter> or;
 }
