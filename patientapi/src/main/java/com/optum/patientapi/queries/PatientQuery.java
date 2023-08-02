fd457ca37a6baf2c9e62f01f7ad8932f2a1a0674
eswaribala <parameswaribala@gmail.com>
2023-08-02 13:07:41 +0530

    updated

--- patientapi/src/main/java/com/optum/patientapi/queries/PatientQuery.java
+++ patientapi/src/main/java/com/optum/patientapi/queries/PatientQuery.java
@@ -1,9 +1,14 @@
 package com.optum.patientapi.queries;
 
+import com.optum.patientapi.dtos.FilterField;
+import com.optum.patientapi.dtos.GenderFilterField;
+import com.optum.patientapi.dtos.PatientFilter;
 import com.optum.patientapi.models.Patient;
+import com.optum.patientapi.repositories.PatientRepo;
 import com.optum.patientapi.services.PatientService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.Column;
@@ -15,6 +20,9 @@ public class PatientQuery implements GraphQLQueryResolver {
 
     @Autowired
     private PatientService patientService;
+
+    @Autowired
+    private PatientRepo patientRepo;
 
     public List<Patient> findAllPatients(){
         return this.patientService.getAllPatients();
@@ -29,5 +37,44 @@ public class PatientQuery implements GraphQLQueryResolver {
     public List<Patient> findPatientByFirstName(String firstName){
 
         return this.patientService.getPatientByFirstName(firstName);
+    }
+
+    public List<Patient> findPatientWithFilter(PatientFilter patientFilter){
+
+        Specification<Patient> spec=null;
+        if(patientFilter.getOpId()!=null){
+
+          spec=byOPId(patientFilter.getOpId());
+        }
+
+        if(patientFilter.getEmail()!=null){
+
+            spec=byEmail(patientFilter.getEmail());
+        }
+
+        if(patientFilter.getGender()!=null){
+
+            spec=byGender(patientFilter.getGender());
+        }
+
+        if(spec!=null)
+           return this.patientRepo.findAll(spec);
+        else
+           return this.patientRepo.findAll();
+
+    }
+
+
+    private Specification<Patient> byOPId(FilterField filterField) {
+        return (Specification<Patient>) (root, query, builder) -> filterField.generateCriteria(builder, root.get("opId"));
+    }
+
+    private Specification<Patient> byEmail(FilterField filterField) {
+        return (Specification<Patient>) (root, query, builder) -> filterField.generateCriteria(builder, root.get("email"));
+
+    }
+
+    private Specification<Patient> byGender(GenderFilterField genderFilterField) {
+        return (Specification<Patient>) (root, query, builder) -> genderFilterField.generateCriteria(builder, root.get("gender"));
     }
 }
